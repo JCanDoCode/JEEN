fa0f89544ca41e6f8770a4e288c29602cce3bc90
JCanDoCode <156962029+JCanDoCode@users.noreply.github.com>
2024-10-05 20:45:08 -0700

    Update 3
    Created checkout page and css. Checkout item container loads local storage bag items, but does not add them up to a grand total yet. Minor fixes to menu responsiveness.

--- js/checkout.js
+++ js/checkout.js
@@ -2,6 +2,8 @@ import items from "./items.js";
 
 const addBtn = document.querySelectorAll(".add-btn");
 const itemCount = document.querySelector("#itemCount");
+const checkoutItemDiv = document.querySelector("#checkoutItems");
+const deleteBtn = document.querySelectorAll(".delete-btn");
 
 const initialization = () => {
     localStorage.setItem("itemNum", 0);
@@ -26,7 +28,24 @@ const addItem = (item) => {
     localStorage.setItem("itemNum", itemNum);
     updateItemAmount();
 }
+const loadItems = () => {
+    if (localStorage.getItem("bagItems")) {
+        let bagItems = JSON.parse(localStorage.getItem("bagItems"));
+        bagItems.forEach(item => {
+            checkoutItemDiv.innerHTML += `<div class="item">
+                                            <div class="name">
+                                                <i class='bx bx-trash-alt delete-btn'></i>
+                                                <p>` + item.name + `</p>
+                                            </div>
+                                            <p>` + item.price + `</p>
+                                        </div>`;
+        });
+    } else {
+        checkoutItemDiv.innerHTML = `<h3>No items in bag</h3><p><a href="./menu.html">Click here</a> to view our menu</p>`
+    }
+}
 
+window.addEventListener("load", loadItems)
 window.addEventListener("load", () => {(localStorage.getItem("itemNum")) ? updateItemAmount() : initialization()});
 addBtn.forEach(btn => {
     const itemName = btn.dataset.item;
